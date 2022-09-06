f49e8a95a4823ae273df29106a249b0a1074b405
dngyals9991 <dngyals9991@gmail.com>
2022-09-06 10:47:42 +0900

    입력을 받아 출력하게 함.

--- 220906/star.c
+++ 220906/star.c
@@ -6,10 +6,17 @@ int main() {
 	scanf_s("d%", &input);
 	for (int = 0; i < input; i++)
 	{
-
-		printf("+");
+		for (int i = 1; i > 0; i--);
+		for (int k = 2 * n - 2 * i; k > 0; k--)printf("");
+		for (int i = i; i > 0; i--)printf("*");
+		printf("\n");
+	}
+	for (int i = 1; i <= n; i++) {
+		for (int i = 1; i <= n; i--)pritnf("*");
+		for (int k = 2 * n - 2 * i; k > 0; k--)pritnf("");
+		for (int i = i; i > 0; i--)printf("*");
+		printf("\n");
 	}
 	return 0;
 
 }
-
